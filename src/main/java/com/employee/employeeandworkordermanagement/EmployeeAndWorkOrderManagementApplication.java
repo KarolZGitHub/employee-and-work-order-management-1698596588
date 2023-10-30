1c5d510964858992f607a6d0a32ac1c7ea63ca24
Karol <karol.zimolag@wp.pl>
2023-10-30 21:31:34 +0100

    Configuration of a basic file structure.

---
+++ src/main/java/com/employee/employeeandworkordermanagement/EmployeeAndWorkOrderManagementApplication.java
@@ -0,0 +1,13 @@
+package com.employee.employeeandworkordermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAndWorkOrderManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeAndWorkOrderManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/employee/employeeandworkordermanagement/EmployeeAndWorkOrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.employeeandworkordermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAndWorkOrderManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
