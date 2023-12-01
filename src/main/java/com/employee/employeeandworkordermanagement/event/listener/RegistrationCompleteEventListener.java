f11c3fc0b52443cfa62a22bd97d7bb4234c396a9
Karol <karol.zimolag@wp.pl>
2023-12-01 21:27:51 +0100

    Setting up email verification feature.

--- src/main/java/com/employee/employeeandworkordermanagement/event/listener/RegistrationCompleteEventListener.java
+++ src/main/java/com/employee/employeeandworkordermanagement/event/listener/RegistrationCompleteEventListener.java
@@ -3,11 +3,17 @@ package com.employee.employeeandworkordermanagement.event.listener;
 import com.employee.employeeandworkordermanagement.event.RegistrationCompleteEvent;
 import com.employee.employeeandworkordermanagement.user.User;
 import com.employee.employeeandworkordermanagement.user.UserService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+
+import java.io.UnsupportedEncodingException;
 import java.util.UUID;
 
 @Component
@@ -15,17 +21,43 @@ import java.util.UUID;
 
 public class RegistrationCompleteEventListener implements ApplicationListener<RegistrationCompleteEvent> {
     private final UserService userService;
+
+    private final JavaMailSender mailSender;
+    private User theUser;
     private static final Logger log = LoggerFactory.getLogger(RegistrationCompleteEventListener.class);
-    @Override
+
     public void onApplicationEvent(RegistrationCompleteEvent event) {
-        //Get user from event
-        User theUser = event.getUser();
-        //Create verification token
+        // 1. Get the newly registered user
+        theUser = event.getUser();
+        //2. Create a verification token for the user
         String verificationToken = UUID.randomUUID().toString();
-        //Save token
+        //3. Save the verification token for the user
         userService.saveUserVerificationToken(theUser, verificationToken);
-        //Build verification token for user
+        //4 Build the verification url to be sent to the user
         String url = event.getApplicationUrl()+"/register/verifyEmail?token="+verificationToken;
+        //5. Send the email.
+        try {
+            sendVerificationEmail(url);
+        } catch (MessagingException | UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
         log.info("Click the link to verify your registration :  {}", url);
+    }
+
+    public void sendVerificationEmail(String url) throws MessagingException, UnsupportedEncodingException {
+        String subject = "Email Verification";
+        String senderName = "Employee Manager registration";
+        String mailContent = "<p> Hi, " + theUser.getFirstName() + ", </p>" +
+                "<p>Thank you for registering at Employee Manager," + "" +
+                "Please, follow the link below to complete your registration.</p>" +
+                "<a href=\"" + url + "\">Verify your email to activate your account</a>" +
+                "<p> Thank you <br> Employee Manager.";
+        MimeMessage message = mailSender.createMimeMessage();
+        var messageHelper = new MimeMessageHelper(message);
+        messageHelper.setFrom("javadeveloper2509@gmail.com", senderName);
+        messageHelper.setTo(theUser.getEmail());
+        messageHelper.setSubject(subject);
+        messageHelper.setText(mailContent, true);
+        mailSender.send(message);
     }
 }
