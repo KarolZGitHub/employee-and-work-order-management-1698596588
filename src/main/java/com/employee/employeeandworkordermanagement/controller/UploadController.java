924ce4727fb4635c90c736e6814112b75eca2364
Karol <karol.zimolag@wp.pl>
2023-12-13 19:31:09 +0100

    Upload photo changes

--- src/main/java/com/employee/employeeandworkordermanagement/controller/UploadController.java
+++ src/main/java/com/employee/employeeandworkordermanagement/controller/UploadController.java
@@ -1,5 +1,6 @@
 package com.employee.employeeandworkordermanagement.controller;
 
+import com.employee.employeeandworkordermanagement.service.UploadPhotoService;
 import com.employee.employeeandworkordermanagement.user.User;
 import com.employee.employeeandworkordermanagement.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -23,8 +24,8 @@ import java.nio.file.Paths;
 @RequiredArgsConstructor
 @RequestMapping("/upload")
 public class UploadController {
-    public static String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/uploads";
-    private final UserRepository userRepository;
+
+    private final UploadPhotoService uploadPhotoService;
 
     @GetMapping("/upload-image")
     public String displayUploadForm() {
@@ -37,18 +38,7 @@ public class UploadController {
             model.addAttribute("msg", "You didn't choose your file.");
             return "upload/uploadImageInformation";
         }
-        StringBuilder fileNames = new StringBuilder();
-        Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, file.getOriginalFilename());
-        fileNames.append(file.getOriginalFilename());
-        Files.write(fileNameAndPath, file.getBytes());
-        User user = userRepository.findByEmail(authentication.getName()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User has not been found."));
-        if (user != null) {
-            user.setProfilePicturePath(fileNameAndPath.toString());
-            userRepository.save(user);
-            model.addAttribute("msg", "Uploaded images: " + fileNames.toString() + " for user: " + user.getFirstName());
-        } else {
-            model.addAttribute("msg", "User not found!");
-        }
+        uploadPhotoService.uploadImage(file,authentication,model);
         return "upload/uploadImageInformation";
     }
 }
--- src/main/java/com/employee/employeeandworkordermanagement/service/UploadPhotoService.java
+++ src/main/java/com/employee/employeeandworkordermanagement/service/UploadPhotoService.java
@@ -1,7 +1,44 @@
 package com.employee.employeeandworkordermanagement.service;
 
+import com.employee.employeeandworkordermanagement.repository.UserRepository;
+import com.employee.employeeandworkordermanagement.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Service
+@RequiredArgsConstructor
 public class UploadPhotoService {
+
+    private final UserRepository userRepository;
+    private static String createUploadDirectory() {
+        String userHome = System.getProperty("user.home");
+        Path uploadPath = Paths.get(userHome, "uploads");
+        return uploadPath.toString();
+    }
+    public void uploadImage(MultipartFile file, Authentication authentication, Model model) throws IOException {
+        System.out.println("Upload directory: " + createUploadDirectory());
+
+        StringBuilder fileNames = new StringBuilder();
+        Path fileNameAndPath = Paths.get(createUploadDirectory(), file.getOriginalFilename());
+        fileNames.append(file.getOriginalFilename());
+        Files.write(fileNameAndPath, file.getBytes());
+        User user = userRepository.findByEmail(authentication.getName()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User has not been found."));
+        if (user != null) {
+            user.setProfilePicturePath(fileNameAndPath.toString());
+            userRepository.save(user);
+            model.addAttribute("msg", "Uploaded images: " + fileNames.toString() + " for user: " + user.getFirstName());
+        } else {
+            model.addAttribute("msg", "User not found!");
+        }
+    }
 }
